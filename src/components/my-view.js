5ead98b32b93cd99abbb34151127db6f6854ba5a
Philipp <philipp.roytburg@rwth-aachen.de>
2019-08-16 15:03:52 +0200

    Update

--- src/components/my-view.js
+++ src/components/my-view.js
@@ -15,7 +15,20 @@ class MyView extends PageViewElement {
       _data: { type: Object },
       _courseId: { type: Number },
       _courseURL: { type: String },
-      _loginSub: { type: String}
+      _loginSub: { type: String},
+      _greenUser: { type: Number },
+      _yellowUser: { type: Number },
+      _redUser: { type: Number },
+      _redLimit: { type: Number},
+      _yellowLimit: { type: Number},
+      _avgScore: { type: Number },
+      _courseDescription: { type: String },
+      _feedback: { type: String },
+      mcService: { type: String },
+      //If you are using different services add a property and configure _updateLearningLocker function
+      moodleDataProxy: { type: String },
+      las2peerBaseURL: { type : String },
+      oidcclientid: { type: String }
     };
   }
 
@@ -73,11 +86,11 @@ class MyView extends PageViewElement {
     
     <div class="vertical-section-container centered" id="widget-container" @signed-in="${this.handleLogin}">
     <las2peer-frontend-statusbar service="Mentoring Cockpit"
-        baseurl="http://tech4comp.dbis.rwth-aachen.de:30014"
-        oidcpopupsigninurl="http://127.0.0.1:8081/node_modules/las2peer-frontend-statusbar/callbacks/popup-signin-callback.html"
-        oidcpopupsignouturl="http://127.0.0.1:8081/node_modules/las2peer-frontend-statusbar/callbacks/popup-signout-callback.html"
-        oidcsilentsigninturl="http://127.0.0.1:8081/node_modules/las2peer-frontend-statusbar/callbacks/silent-callback.html"
-        oidcclientid="b63cfd8b-9cd2-4c87-9dce-4e6ed90d2d45"
+        baseurl="${this.las2peerBaseURL}"
+        oidcpopupsigninurl="./node_modules/las2peer-frontend-statusbar/callbacks/popup-signin-callback.html"
+        oidcpopupsignouturl="./node_modules/las2peer-frontend-statusbar/callbacks/popup-signout-callback.html"
+        oidcsilentsigninturl="./node_modules/las2peer-frontend-statusbar/callbacks/silent-callback.html"
+        oidcclientid="${this.oidcclientid}"
         autoAppendWidget=true
         @signed-out="${this.handleLogout}"
       ></las2peer-frontend-statusbar>
@@ -98,7 +111,6 @@ class MyView extends PageViewElement {
       item-value-path="_id" 
       item-label-path="name">
       </vaadin-combo-box>
-      <a href="${this._email}">${this._email}</a>
       
     </section>
 
@@ -119,35 +131,87 @@ class MyView extends PageViewElement {
         rows='${this._data}'>
       </google-chart>
     </section>
+    <section>
+    <h2>Course Statistics:</h2>
+    <ul style="list-style-type:none;">
+      <li>Green Students: ${this._greenUser}</li>
+      <li>Yellow Students: ${this._yellowUser}</li>
+      <li>Red Students: ${this._redUser}</li>
+      <li>Average Score: ${this._avgScore}</li>
+    </ul>
+    </section>
+
+    <section>
+    <h2>Problems:</h2>
+    <p>${this._courseDescription}</p>
+    <p>${this._feedback}</p>
+    <ul id = "feedbackList">
+    </ul>
+    <p>Contact the student under: <a href="${this._email}">${this._email}</a><p>
+    </section>
+
+
     `
   }
 
   constructor() {
     super();
-    //his._courseId = 3;
     this._data = '[]';
-    //this.fetchPersona();
-    //this.fetchStatements();
+    this._email = '';
+    //this.setEverythingToZero();
+    this._redLimit = 0.5;
+    this._yellowLimit = 0.6;
+
+    
+    //read form properties file
+    fetch('etc/config.properties')
+      .then(res => res.text())
+      .then(text => {
+        //get rid of whitespace
+        const config = text.trim()
+          //split into an array of properties
+          .split('\n')
+          //get rid of spaces and split each property into an array
+          .map(m => m.replace(/\s+/g, '').split('='))
+          //transform the array into a dictionary
+          .reduce(function(map, obj){
+            map[obj[0]] = obj[1];
+            return map;
+          }, {});
+        this.mcService = config['mcService'];
+        this.moodleDataProxy =config['moodleDataProxy'];
+        this.las2peerBaseURL = config['las2peerBaseURL'];
+        this.oidcclientid = config['oidcclientid'];
+      });
+    
   }
+
 
   fetchStatements() {
     //aggregation http interface
-    //TODO login with token
     const courseURL_Encoded = this.encode(this._courseURL);
-    const uri = `http://137.226.232.175:31016/mentoring/${this._loginSub}/${courseURL_Encoded}/results`;
+    const uri = `${this.mcService}/${this._loginSub}/${courseURL_Encoded}/results`;
     fetch(uri, {method: 'GET',
       'Content-Type': 'application/json'
     })
       .then(res => res.json())
-      .then(json => this._results = json)
+      .then(json => {
+        this._results = json;
+        const scores = json.map(e => e.averageScore);
+        scores.forEach(score => {
+          if (this.getLightsColor(score) === 'red') this._redUser++;
+          else if (this.getLightsColor(score) === 'yellow') this._yellowUser++;
+          else if (this.getLightsColor(score) === 'green') this._greenUser++;
+        });
+        this._avgScore = scores.reduce((score1, score2) => score1 + score2)*100/scores.length;
+      })
       .catch(() => console.log("Service unavailable"));
   }
 
   fetchPersona() {
     //aggregation http interface
-    //TODO login with token
     const courseURL_Encoded = this.encode(this._courseURL);
-    const uri = `http://137.226.232.175:31016/mentoring/${this._loginSub}/${courseURL_Encoded}/students`;
+    const uri = `${this.mcService}/${this._loginSub}/${courseURL_Encoded}/students`;
     fetch(uri, {method: 'GET',
       'Content-Type': 'application/json'
     })
@@ -164,9 +228,9 @@ class MyView extends PageViewElement {
             // only one can be selected and target.value is the id, so [0] selects the first and only user
             const userInfo = this._results.filter(user => user._id === e.target.value)[0];
             // display results
+            this._feedback = this.getProblems(userInfo.averageScore, userInfo.results)
             this.setLightsColor(userInfo.averageScore, true);
             this._data = JSON.stringify(userInfo.results.map(r => [r.name, r.score*100]));
-            //console.log(temp);
           }
         })
       })
@@ -174,35 +238,67 @@ class MyView extends PageViewElement {
   }
 
   setLightsColor(avgScore, on) {
-    if(on && avgScore < 0.5) {
-      this.shadowRoot.getElementById('redLight').style.backgroundColor = "red";
-      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = "#111";
-      this.shadowRoot.getElementById('greenLight').style.backgroundColor = "#111";
-    } else if(on && avgScore < 0.6) {
-      this.shadowRoot.getElementById('redLight').style.backgroundColor = "#111";
-      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = "yellow";
-      this.shadowRoot.getElementById('greenLight').style.backgroundColor = "#111";
+    if(on && avgScore < this._redLimit) {
+      this.shadowRoot.getElementById('redLight').style.backgroundColor = 'red';
+      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = '#111';
+      this.shadowRoot.getElementById('greenLight').style.backgroundColor = '#111';
+    } else if(on && avgScore < this._yellowLimit) {
+      this.shadowRoot.getElementById('redLight').style.backgroundColor = '#111';
+      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = 'yellow';
+      this.shadowRoot.getElementById('greenLight').style.backgroundColor = '#111';
     } else if(on) { 
-      this.shadowRoot.getElementById('redLight').style.backgroundColor = "#111";
-      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = "#111";
+      this.shadowRoot.getElementById('redLight').style.backgroundColor = '#111';
+      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = '#111';
       this.shadowRoot.getElementById('greenLight').style.backgroundColor = "green";
     } else {
-      this.shadowRoot.getElementById('redLight').style.backgroundColor = "#111";
-      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = "#111";
-      this.shadowRoot.getElementById('greenLight').style.backgroundColor = "#111";
+      this.shadowRoot.getElementById('redLight').style.backgroundColor = '#111';
+      this.shadowRoot.getElementById('yellowLight').style.backgroundColor = '#111';
+      this.shadowRoot.getElementById('greenLight').style.backgroundColor = '#111';
     }
   }
 
+  getLightsColor(avgScore) {
+    if(avgScore < this._redLimit) return 'red';
+    else if(avgScore < this._yellowLimit) return 'yellow';
+    else return 'green';
+  }
+
+  getProblems(avgScore, results) {
+    this._courseDescription = results[0].description.split('\n')[0];
+    var feedback;
+    const feedbackList = this.shadowRoot.getElementById('feedbackList');
+    feedbackList.innerText = '';
+    if(avgScore < this._redLimit){
+      
+      results.forEach(r => {
+        if(r.score < this._yellowLimit) {
+          feedback = r.description.split('\n');
+          feedbackList.innerHTML += `<li>${r.name}: ${feedback[1]}</li>\n`;
+        }
+      });
+      //feedbackList.innerHTML = temp;
+      return 'The student needs help. He has problems with following assignments:';
+    } 
+    else if(avgScore < this._yellowLimit){
+      results.forEach(r => {
+        if(r.score < this._yellowLimit) {
+          feedback = r.description.split('\n');
+          feedbackList.innerHTML += `<li>${r.name}: ${feedback[1]}</li>\n`;
+        }
+      });
+      return 'The student may need some assisstens with following assignments: \n'.concat(feedback);
+    }
+    else return 'It seems, that the student doesn\'t need additional help.';
+  }
+
   _updateLearningLocker() {
-    fetch(`http://tech4comp.dbis.rwth-aachen.de:31012/mc/moodle-data/${this._courseId}`, {method: 'POST'})
+    fetch(`${this.moodleDataProxy}/${this._courseId}`, {method: 'POST'})
     .catch(() => console.log("Update failed"));
     const combos = this.shadowRoot.querySelectorAll('vaadin-combo-box');
     combos.forEach(comboBox => comboBox.value = '');
     combos[1].items = null;
-    this._email = '';
-    this.setLightsColor(0, false);
     this._data = '[]';
-
+    this.setEverythingToZero();
     this.fetchPersona();
     this.fetchStatements();;
     //location.reload();
@@ -210,7 +306,7 @@ class MyView extends PageViewElement {
   
   handleLogin(event) {
     this._loginSub = event.detail.profile.sub;
-    const uri = `http://137.226.232.175:31016/mentoring/${this._loginSub}/courseList`
+    const uri = `${this.mcService}/${this._loginSub}/courseList`
     fetch(uri, {method: 'GET',
       'Content-Type': 'application/json'
     })
@@ -223,21 +319,33 @@ class MyView extends PageViewElement {
           this._courseURL = e.target.value;
           this._courseId = this._courseURL.split('id=').pop();
           combos[1].value = '';
+          this.setEverythingToZero();
           this.fetchPersona();
           this.fetchStatements();;
         });
       });
   }
 
+  setEverythingToZero() {
+    this._greenUser = 0;
+    this._yellowUser = 0;
+    this._redUser = 0;
+    this._avgScore = 0;
+    this._email = '';
+    this._courseDescription = '';
+    this._feedback = '';
+    this.setLightsColor(0, false);
+    this.shadowRoot.getElementById('feedbackList').innerHTML = '';
+
+  }
+
   handleLogout() {
-    //console.log('test');
     const combos = this.shadowRoot.querySelectorAll('vaadin-combo-box');
     combos.forEach(comboBox => {
       comboBox.value = '';
       comboBox.items = null;
     });
-    this._email = '';
-    this.setLightsColor(0, false);
+    this.setEverythingToZero();
     this._data = '[]';
   }
 
@@ -247,7 +355,7 @@ class MyView extends PageViewElement {
     for (var i = 0; i < length; i++)
         number += string.charCodeAt(i).toString(16);
     return number;
-}
+  }
 
 }
 
